7d95efdd9a7be2ad857c303cf050accba23d5ff9
AswanthAnu <aswanthanu777@gmail.com>
2022-08-05 14:31:49 +0530

    first commit

---
+++ ecom/homeapp/views.py
@@ -0,0 +1,8 @@
+from ast import Return
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("hello")
