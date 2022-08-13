7b5ea3bc2607cc5fc896ea74fcbfe692fc9cab7e
AswanthAnu <aswanthanu777@gmail.com>
2022-08-14 02:22:13 +0530

    django project cart remove and rendering empty page

--- ecom/carts/views.py
+++ ecom/carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from store.models import product
 from django.core.exceptions import ObjectDoesNotExist
@@ -82,3 +82,28 @@ def cart(request, total = 0, quantity = 0 , cart_item = None):
     }
 
     return render(request, 'store/cart.html', context)
+
+
+def remove_cart(request, product_id):
+
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    prod = get_object_or_404(product, id = product_id)
+    cart_item = CartItem.objects.get(product = prod, cart = cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+        context = {
+            'quantity' : cart_item.quantity
+        }
+        
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id = _cart_id(request))
+    prod = get_object_or_404(product, id = product_id)
+    cart_item = CartItem.objects.get(product = prod, cart = cart)
+    cart_item.delete()
+    return redirect('cart')
