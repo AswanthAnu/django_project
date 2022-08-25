9099ae84cddb2f28d2be4ae46237d5f16ce1b36a
AswanthAnu <aswanthanu777@gmail.com>
2022-08-25 21:44:26 +0530

    search complete

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     path('category/<slug:category_slug>',views.store, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:sub_category_slug>',views.store, name='products_by_sub_category'),
 
-
-
     path('category/<slug:brand_slug>',views.store, name='products_by_brand'),
 
-    
     path('category/<slug:category_slug>/<slug:product_slug>',views.product_detail, name='product_detail'),
     path('category/<slug:category_slug>/<slug:subcategory_slug>/<slug:product_slug>',views.product_detail, name='product_detail'),
 
